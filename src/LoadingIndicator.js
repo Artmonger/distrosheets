5c9a984efa06634aec3c6c3fad8dbd1761984490
Cameron Smith <cameronsmith@Camerons-Air.lan>
2025-05-21 09:35:29 -0400

    Use resizing.gif as the spinner during image resizing overlay

--- src/LoadingIndicator.js
+++ src/LoadingIndicator.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LoadingIndicator = ({ message }) => (
   <div className="loading">
-    <img src="/monday-spinner.gif" alt="Loading..." style={{ width: 80, height: 80, marginBottom: 24 }} />
+    <img src="/resizing.gif" alt="Loading..." style={{ width: 80, height: 80, marginBottom: 24 }} />
     {message && <p>{message}</p>}
   </div>
 );
